0a9e80daf212aca421e9b4eacec162de9dfe6b07
Mitan <mitantank00@gamil.com>
2024-05-17 11:51:55 +0530

    DataTypes

---
+++ DataTypes.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main(){
+    int a = 10;
+    float b = 10.5;
+    double c = 10.5;
+    char d = 'a';
+    printf("%d\n",a);
+    printf("%f\n",b);
+    printf("%lf\n",c);
+    printf("%c\n",d);
+    a = 20;
+    printf("%d\n",a);
+}
